1be216324da134d5388e50584fcd49f005205946
Tess Goossens <70571136+TessGoossens@users.noreply.github.com>
2022-04-05 19:49:50 +0200

    #

---
+++ tracks/src/screens/SignupScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet, Text, Button } from 'react-native';
+
+const SignupScreen = ({ navigation }) => {
+    return (
+    <>
+        <Text style={{fontSize: 48}}>Signupt Screen</Text>
+        <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')}/>
+        <Button title="Go to main flow" onPress={() => navigation.navigate('mainFlow')}/>
+    </>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
